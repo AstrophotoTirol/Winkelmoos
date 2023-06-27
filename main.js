9a4eccd5a1e60de1303434308f2db8ba278b688e
FabianWild <Fabian.Wild1996@gmx.de>
2023-06-27 18:45:42 +0200

    forecast

--- main.js
+++ main.js
@@ -13,7 +13,8 @@ let map = L.map("map", {
 
 // thematische Layer
 let themaLayer = {
-    route: L.featureGroup()
+    route: L.featureGroup(),
+    forecast: L.featureGroup()
 }
 
 // WMTS Hintergrundlayer der eGrundkarte Tirol definieren
@@ -32,7 +33,8 @@ let eGrundkarteTirol = {
     nomenklatur: L.tileLayer("https://wmts.kartetirol.at/gdi_nomenklatur/{z}/{x}/{y}.png", {
         attribution: `Datenquelle: <a href="https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol">eGrundkarte Tirol</a>`,
         pane: "overlayPane",
-    })
+    }),
+    
 }
 
 // Hintergrundlayer eGrundkarte Tirol mit GPX overlay
@@ -50,8 +52,8 @@ let layerControl = L.control.layers({
         eGrundkarteTirol.nomenklatur,
     ])
 }, {
-    "Zustiegsroute zum Photospot": themaLayer.route.addTo(map)
-}).addTo(map);
+    "Zustiegsroute zum Photospot": themaLayer.route.addTo(map),
+    "Vorhersage durch Klicken auf Karte": themaLayer.forecast.addTo(map)}).addTo(map);
 
 // Maßstab
 L.control.scale({
@@ -66,3 +68,52 @@ let controlElevation = L.control.elevation({
     theme: "zustieg"
 }).addTo(map);
 controlElevation.load("./data/winklmoos.gpx")
+
+
+// Wettervorhersage MET Norway
+async function showForecast(url, latlng) {
+    let response = await fetch(url);
+    let jsondata = await response.json();
+    //console.log(jsondata, latlng);
+
+    let current = jsondata.properties.timeseries[0].data.instant.details;
+    //console.log(current);
+    let timestamp =new Date(jsondata.properties.meta.updated_at).toLocaleString();
+    let timeseries = jsondata.properties.timeseries;
+
+    let markup = `
+        <h4>Wetter für ${latlng.lat.toFixed(4)}, ${latlng.lng.toFixed(4)} (${timestamp})</h4>
+        <table>
+            <tr><td>Luftdruck (hPa)</td><td>${current.air_pressure_at_sea_level}</td></tr>
+            <tr><td>Lufttemperatur (°C)</td><td>${current.air_temperature}</td></tr>
+            <tr><td>Bewölkungsgrad (%)</td><td>${current.cloud_area_fraction}</td></tr>
+            <tr><td>Relative Luftfeuchtigkeit (%)</td><td>${current.relative_humidity}</td></tr>
+            <tr><td>Windrichtung (°)</td><td>${current.wind_from_direction}</td></tr>
+            <tr><td>Windgeschwindigkeit (m/s)</td><td>${current.wind_speed}</td></tr>
+        </table>
+    `;
+
+    // Wettersymbole hinzufügen
+    for (let i=0; i<=24; i+=3) {
+        //console.log(timeseries[i]);
+        let icon = timeseries[i].data.next_1_hours.summary.symbol_code;
+        let image = `icons/${icon}.svg`;
+        markup += `<img src="${image}" style="width:32px;" title="${timeseries[i].time.toLocaleString()}">`
+        //console.log(icon, image);
+    }
+
+    L.popup().setLatLng(latlng).setContent(markup).openOn(themaLayer.forecast);
+
+}
+
+// auf Kartenklick reagieren
+map.on("click", function(evt) {
+    //console.log(evt.latlng.lat);
+    let url = `https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=${evt.latlng.lat}&lon=${evt.latlng.lng}`;
+    showForecast(url, evt.latlng);
+});
+
+// Klick auf Innsbruck simulieren
+map.fireEvent("click", {
+    latlng: L.latLng(ibk.lat, ibk.lng)
+})
